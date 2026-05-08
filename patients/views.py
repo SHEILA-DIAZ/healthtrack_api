eda7839416af362a86ac72080b704d6c6dd81bee
Sheila Diaz Rojas <sheila.diaz@tecsup.edu.pe>
2026-05-08 14:56:43 -0500

    update views

--- patients/views.py
+++ patients/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Paciente, Doctor
 from .serializers import PacienteSerializer, DoctorSerializer
 
+# views api
 class PacienteViewSet(viewsets.ModelViewSet):
     queryset = Paciente.objects.all()
     serializer_class = PacienteSerializer
